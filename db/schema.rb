0a1222e7e7c852aa8e7e5f79c160504608895690
ikehiro1 <ikegami0922hiroki@gmail.com>
2023-12-23 15:46:53 +0900

    route一部設定

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,24 @@
 
 ActiveRecord::Schema[7.1].define(version: 2023_12_22_044100) do
   create_table "orders", force: :cascade do |t|
+    t.string "order_number1"
+    t.string "order_number2"
+    t.string "order_number3"
+    t.string "order_number4"
+    t.string "order_number5"
+    t.string "order_number6"
+    t.string "order_number7"
+    t.string "order_number8"
+    t.string "order_number9"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "teams", force: :cascade do |t|
+    t.string "team_name"
+    t.string "team_number"
+    t.string "team_img"
+    t.string "schedule"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -27,6 +40,15 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_22_044100) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name"
+    t.string "position"
+    t.string "number"
+    t.string "password"
+    t.string "mail"
+    t.string "hand"
+    t.string "batting"
+    t.string "introduction"
+    t.string "prayer_img"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
