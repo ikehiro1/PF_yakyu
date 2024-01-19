bfcd1c812aaa4774ee1252c02349bd3e6be12602
ikehiro1 <ikegami0922hiroki@gmail.com>
2024-01-19 15:05:53 +0900

    お問い合わせ確認

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     domain: 'gmail.com',
     port: 587,
-    user_name: ENV['GMAIL_ADDRESS'],
+    user_name: "okashifuguozi@gmail.com",
     password: ENV['GMAIL_PASSWORD'],
     authentication: 'plain',
     enable_starttls_auto: true
